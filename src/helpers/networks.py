ec1b19f5b1d17d9d2d5bd9efbf1ca7063c4c3e1f
Pratim Chowdhary <cpratim@macbookpro.lan>
2024-12-30 16:59:38 -0500

    init

--- src/helpers/networks.py
+++ src/helpers/networks.py
@@ -33,7 +33,6 @@ class FullyConnectedOneLayer(nn.Module):
         return h1, out
     
 
-
 class FullyConnectedTwoLayer(nn.Module):
 
     def __init__(self, input_size, hidden_size, num_classes):
@@ -60,7 +59,59 @@ class FullyConnectedTwoLayer(nn.Module):
         h2 = self.relu(out)
         out = self.fc3(h2)
         return h1, h2, out
-        
+    
+
+class LeNet5(nn.Module):
+    
+    def __init__(self, num_classes, input_size, apply_softmax=False):
+        C, H, W = input_size
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(C, 6, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=2)
+
+        output_size = ((H - 5 + 1) // 2 - 5 + 1) // 2
+
+        self.fc1 = nn.Linear(16 * output_size * output_size, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1) if apply_softmax else None
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu(out)
+        out = self.max_pool(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.max_pool(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        if self.softmax is not None:
+            out = self.softmax(out)
+        return out
+
+    def forward_return_hidden(self, x):
+        out = self.conv1(x)
+        h1 = self.relu(out)
+        out = self.max_pool(h1)
+        out = self.conv2(out)
+        h2 = self.relu(out)
+        out = self.max_pool(h2)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        h3 = self.relu(out)
+        out = self.fc2(h3)
+        h4 = self.relu(out)
+        out = self.fc3(h4)
+        if self.softmax is not None:
+            out = self.softmax(out)
+        return h1, h2, h3, h4, out
+
 
 def load_fully_connected_two_layer(input_size, hidden_size, num_classes, learning_rate):
     model = FullyConnectedTwoLayer(input_size, hidden_size, num_classes)
@@ -68,9 +119,14 @@ def load_fully_connected_two_layer(input_size, hidden_size, num_classes, learnin
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     return model, criterion, optimizer
 
-
 def load_fully_connected_one_layer(input_size, hidden_size, num_classes, learning_rate):
     model = FullyConnectedOneLayer(input_size, hidden_size, num_classes)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    return model, criterion, optimizer
+
+def load_lenet5(num_classes, input_size, learning_rate):
+    model = LeNet5(num_classes, input_size)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     return model, criterion, optimizer
